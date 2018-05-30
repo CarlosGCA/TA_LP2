88d35c47c8b7aa8ced3671eabafb45ac8f6ec655
Carlos Carrillo <carlosg.carrillo@pucp.pe>
2018-05-30 02:12:39 -0500

    Registro Pedidos implementado

--- TA_LP2/src/Email/Email.java
+++ TA_LP2/src/Email/Email.java
@@ -2,46 +2,46 @@ package Email;
 
 import java.util.ArrayList;
 import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-/**
- *
- * @author alfredo
- */
-public class Email {
-    private Properties mailServerProperties;
-    private Session getMailSession;
-    private MimeMessage generateMailMessage;
-
-    public void prepareConection() {
-
-        mailServerProperties = System.getProperties();
-        mailServerProperties.put("mail.smtp.port", "587");
-        mailServerProperties.put("mail.smtp.auth", "true");
-        mailServerProperties.put("mail.smtp.starttls.enable", "true");
-      
-    }
-
-   public  void sendEmail(String subject,String emailToSend, String toEmail ) throws AddressException, MessagingException {
-
-        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
-        generateMailMessage = new MimeMessage(getMailSession);
-        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
-       
-        
-        generateMailMessage.setSubject(subject);        
-        generateMailMessage.setContent(emailToSend, "text/html");
-        try (Transport transport = getMailSession.getTransport("smtp")) {
-            // Aca va el correo y contrasena , estaba con el mio , pero por obvias razones lo saque 
-            transport.connect("smtp.gmail.com", "", "");
-            transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-        }
-    }
-}
+//import javax.mail.Message;
+//import javax.mail.MessagingException;
+//import javax.mail.Session;
+//import javax.mail.Transport;
+//import javax.mail.internet.AddressException;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
+///**
+// *
+// * @author alfredo
+// */
+//public class Email {
+//    private Properties mailServerProperties;
+//    private Session getMailSession;
+//    private MimeMessage generateMailMessage;
+//
+//    public void prepareConection() {
+//
+//        mailServerProperties = System.getProperties();
+//        mailServerProperties.put("mail.smtp.port", "587");
+//        mailServerProperties.put("mail.smtp.auth", "true");
+//        mailServerProperties.put("mail.smtp.starttls.enable", "true");
+//      
+//    }
+//
+//   public  void sendEmail(String subject,String emailToSend, String toEmail ) throws AddressException, MessagingException {
+//
+//        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+//        generateMailMessage = new MimeMessage(getMailSession);
+//        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+//       
+//        
+//        generateMailMessage.setSubject(subject);        
+//        generateMailMessage.setContent(emailToSend, "text/html");
+//        try (Transport transport = getMailSession.getTransport("smtp")) {
+//            // Aca va el correo y contrasena , estaba con el mio , pero por obvias razones lo saque 
+//            transport.connect("smtp.gmail.com", "", "");
+//            transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+//        }
+//    }
+//}
 
 
